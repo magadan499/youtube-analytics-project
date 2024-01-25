9d394804c4ed6a5d207c044a5a815a3d08c5811f
Maksim Toguzov <magadan499@mail.ru>
2024-01-25 15:04:53 +0300

    add print_info and fix init

--- src/channel.py
+++ src/channel.py
@@ -12,10 +12,21 @@ class Channel:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
+        self.videoCount = channel['items'][0]['statistics']['videoCount']
+        self.viewCount = channel['items'][0]['statistics']['viewCount']
 
     def printj(self, dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале."""
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.printj(channel)
 
     @classmethod
     def get_service(cls):
@@ -26,4 +37,4 @@ class Channel:
 
 
 moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-print(moscowpython.printj())
+print(moscowpython.print_info())
