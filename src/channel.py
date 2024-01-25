8aa81e0f0f8477b66d2bcd8121147404088a6fc3
Maksim Toguzov <magadan499@mail.ru>
2024-01-25 15:40:04 +0300

    fix channel (to_json and init)

--- src/channel.py
+++ src/channel.py
@@ -15,9 +15,9 @@ class Channel:
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = f"https://www.youtube.com/channel/{self.channel_id}"
-        self.subscriberCount = channel['items'][0]['statistics']['subscriberCount']
-        self.videoCount = channel['items'][0]['statistics']['videoCount']
-        self.viewCount = channel['items'][0]['statistics']['viewCount']
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def printj(self, dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
@@ -41,9 +41,9 @@ class Channel:
                 'title': self.title,
                 'description': self.description,
                 'url': self.url,
-                'subscriber_count': self.subscriberCount,
-                'video_count': self.videoCount,
-                'view_count': self.viewCount
+                'subscriber_count': self.subscriber_count,
+                'video_count': self.video_count,
+                'view_count': self.view_count
                 }
         with open(file_name, 'w', encoding='UTF-8') as file:
             json.dump(data, file)
