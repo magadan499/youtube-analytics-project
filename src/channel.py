ca511ece4ce2da4d34903876f751924f97117757
Maksim Toguzov <magadan499@mail.ru>
2024-01-25 14:46:44 +0300

    add printj and fix init

--- src/channel.py
+++ src/channel.py
@@ -11,6 +11,11 @@ class Channel:
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
+        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+
+    def printj(self, dict_to_print: dict) -> None:
+        """Выводит словарь в json-подобном удобном формате с отступами"""
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     @classmethod
     def get_service(cls):
@@ -21,4 +26,4 @@ class Channel:
 
 
 moscowpython = Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-print(moscowpython.get_service())
+print(moscowpython.printj())
