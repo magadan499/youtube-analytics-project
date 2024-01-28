259022656e8f8ab8decdbebbac34669cc910dd63
Maksim Toguzov <magadan499@mail.ru>
2024-01-28 11:26:09 +0300

    fix channel_id private

--- src/channel.py
+++ src/channel.py
@@ -10,11 +10,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        self.__channel_id = channel_id
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
-        self.url = f"https://www.youtube.com/channel/{self.channel_id}"
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
         self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
@@ -25,7 +25,7 @@ class Channel:
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        channel = self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        channel = self.youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         self.printj(channel)
 
     @classmethod
@@ -38,7 +38,7 @@ class Channel:
 
     def to_json(self, file_name):
         """Сохраняет в Json файл значения атрибутов экземпляра - информацию о канале"""
-        data = {'channel_id': self.channel_id,
+        data = {'channel_id': self.__channel_id,
                 'title': self.title,
                 'description': self.description,
                 'url': self.url,
