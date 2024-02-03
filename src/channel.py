81ad74b9c1913a17dc598b800abab34dda536bc9
Maksim Toguzov <magadan499@mail.ru>
2024-02-03 10:20:30 +0300

    add addition and subtraction

--- src/channel.py
+++ src/channel.py
@@ -14,12 +14,20 @@ class Channel:
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
-        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
 
     def __str__(self):
         return f"{self.title} {self.url}"
+
+    def __add__(self, other):
+        """Сложение количества подписчиков"""
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        """Вычитание количества подписчиков"""
+        return self.subscriber_count - other.subscriber_count
 
     def printj(self, dict_to_print: dict) -> None:
         """Выводит словарь в json-подобном удобном формате с отступами"""
