c7dfdeabb19c17016a4b43cd8123abb40a069300
Maksim Toguzov <magadan499@mail.ru>
2024-02-07 21:59:26 +0300

    add class PLVideo

--- src/video.py
+++ src/video.py
@@ -4,12 +4,11 @@ from src.channel import Channel
 from googleapiclient.discovery import build
 
 
-
 class Video:
     api_key = os.getenv('YOUTUBE_API_KEY')
 
     def __init__(self, video_id: str) -> None:
-        """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+        """Инициализирует id видео, название, ссылку на видео, количество просмотров и лайков."""
         self.video_id = video_id
         self.video_response = self.get_service().videos().list(id=self.video_id, part='snippet,statistics,contentDetails,topicDetails').execute()
         self.title = self.video_response['items'][0]['snippet']['title']
@@ -18,7 +17,7 @@ class Video:
         self.like_count = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        """Вывод названия канала и ссылки на канал"""
+        """Вывод названия видео"""
         return self.title
 
     def printj(self, dict_to_print: dict) -> None:
@@ -26,7 +25,7 @@ class Video:
         print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
+        """Выводит в консоль информацию о видео."""
         self.video_response = self.get_service().videos().list(id=self.video_id, part='snippet,statistics,contentDetails,topicDetails').execute()
         self.printj(self.video_response)
 
@@ -34,3 +33,11 @@ class Video:
     def get_service(cls):
         """Возвращает объект для работы с YouTube API"""
         return build('youtube', 'v3', developerKey=Channel.api_key)
+
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id):
+        """Инициализирует id видео, id плейлиста,
+        наследует аттрибуты инициализации из класса Video"""
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
